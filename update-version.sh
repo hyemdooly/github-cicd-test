10767f95dbaed6dcafa54f3e660def892dda29ad
Song Hyemin <hyemdooly@gmail.com>
2024-08-19 16:26:56 +0900

    Update update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -1,22 +1,30 @@
-# update version
-# versionCode, versionName 변경
+# 기존 version code 추출
+OLD_APP_VERSION_CODE=$(grep '^appVersionCode=' appVersion.properties | cut -d'=' -f2)
+
+# branch로부터 versionCode, versionName 변경
 export APP_VERSION_NAME=`git branch --show-current | grep -Eo '\b[0-9]+\.[0-9]+\.[0-9]+\b'`
 
 # version 추출
 IFS='.'
 read -r MAJOR MINOR PATCH <<< "$APP_VERSION_NAME"
-APP_VERSION_CODE=$((MAJOR * 10000 + MINOR * 100 + PATCH))
+NEW_APP_VERSION_CODE=$((MAJOR * 100000 + MINOR * 1000 + PATCH * 10))
 IFS=' '
 
-echo "APP_VERSION_CODE=$APP_VERSION_CODE"
-echo "APP_VERSION_NAME=$APP_VERSION_NAME"
+# OLD_VERSION_CODE와 NEW_APP_VERSION_CODE 비교
+if [ "$OLD_APP_VERSION_CODE" -eq "$NEW_APP_VERSION_CODE" ]; then
+  NEW_APP_VERSION_CODE=$((NEW_APP_VERSION_CODE + 1))
+fi
+
+echo "OLD_APP_VERSION_CODE=$OLD_APP_VERSION_CODE"
+echo "NEW_APP_VERSION_CODE=$NEW_APP_VERSION_CODE"
+echo "NEW_APP_VERSION_NAME=$NEW_APP_VERSION_NAME"
 
 cat << EOF > appVersion.properties
-appVersionCode=$APP_VERSION_CODE
-appVersionName=$APP_VERSION_NAME
+appVersionCode=$NEW_APP_VERSION_CODE
+appVersionName=$NEW_APP_VERSION_NAME
 EOF
 
 # 커밋 & 푸시
 git add .
-git commit -m "Update Version to $APP_VERSION_NAME"
+git commit -m "Update Version to $NEW_APP_VERSION_NAME"
 git push origin
